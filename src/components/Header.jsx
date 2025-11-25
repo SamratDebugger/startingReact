34d39e23499e256edb9f6ae7bcf475788138f30b
Samrat-Debugger <md721023@gmail.com>
2025-11-26 01:33:01 +0600

    background colour with react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
-import { Menu } from "lucide-react"
-import { Aside } from "./Aside"
-import { useState } from "react"
-
+import { Menu } from "lucide-react";
+import { Aside } from "./Aside";
+import { useState } from "react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isRed, setIsRed] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false)
-    console.log(isOpen);
+  if (isRed) {
+    document.body.classList.add("bgRed");
+  } else {
+    document.body.classList.remove("bgRed");
+  }
+  return (
+    <header className="nav">
+      <Aside isOpen={isOpen} setIsOpen={setIsOpen} />
 
-    return (
-        <header className="nav">
-            <Aside isOpen={isOpen} setIsOpen={setIsOpen} />
-
-            <button onClick={() => setIsOpen(true)} id="menuBtn"><Menu /> </button>
-            <button id="button" className="btn">click here</button>
-        </header>
-    )
+      <button onClick={() => setIsOpen(true)} id="menuBtn">
+        <Menu />{" "}
+      </button>
+      <button
+        onClick={() => setIsRed((prev) => !prev)}
+        id="button"
+        className="btn"
+      >
+        click here
+      </button>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
