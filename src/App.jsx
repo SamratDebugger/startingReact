e842e85307fe139822012b25bedb36a56b9b52c6
Samrat-Debugger <md721023@gmail.com>
2025-12-02 01:51:15 +0600

    set useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Header from "./components/Header"
+import { useState } from "react";
+import Card from "./components/Card";
+import Header from "./components/Header";
 
 function App() {
-
-
+  // set useState
+  const [cardData, setCardData] = useState(null);
   return (
-    <Header />
-  )
+    <>
+      <Header />
+      <section className="cards">
+        {Array.from({ length: 5 }).map((_, i) => (
+          <Card key={i} />
+        ))}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import "./card.css";
+export default function Card() {
+  return (
+    <div className="card">
+      <h2>Card Tittle</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur
+        tempora dignissimos ratione aut atque provident adipisci magnam quis
+        quaerat eos possimus saepe consequatur facere sapiente, doloremque dolor
+        cumque expedita aliquam?
+      </p>
+    </div>
+  );
+}
